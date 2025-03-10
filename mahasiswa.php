6c0989edef6c4f0d08900669badb8c843f904278
valiriusnazara <valiriusnazara@gmail.com>
2025-03-10 11:47:35 +0700

    Create mahasiswa.php
    Buat modu mahasiswa

---
+++ mahasiswa.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h6>Data Mahasiswa AMP</h6>
+    <hr>
+    <h6>NIM : 111111</h6>
+    <h6>Nama : Valirius Nazara</h6><br> 
+    <a href="index.php">Kembali</a>
+</body>
+</html>
