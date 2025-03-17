02eae18f46f8e4e1fa35a315b16c9a44c0d90738
valiriusnazara <valiriusnazara@gmail.com>
2025-03-17 11:12:12 +0700

    Create dosen.php
    data dosen

---
+++ dosen.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Dosen</title>
+</head>
+<body>
+    <h6>Data Dosen AMP</h6>
+    <hr>
+    <h6>NIDS : 200000</h6>
+    <h6>Nama : Ulfahri.M.Kom</h6><br> 
+    <h6>Tahun 2025</h6>
+    <h6>Kampus</h6>   
+    <h6>AMIK MAHAPUTRA RIAU</h6>
+    <h6>Kota : Pekanbaru</h6>
+    <a href="index.php">Kembali</a>
+</body>
+</html>
