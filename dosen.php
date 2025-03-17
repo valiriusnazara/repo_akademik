4d3796c924becfe4225a724765cfff02c6546649
mei003 <menovita1205@gmail.com>
2025-03-17 11:11:27 +0700

    Create dosen.php
    data dosen

---
+++ dosen.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATA DOSEN</title>
+</head>
+<body>
+    <h6>Data Dosen</h6>
+    <hr>
+    <h6>NIM: 6789</h6>
+    <h6>NAMA: Darmanta sukrianto</h6>
+    <h6>Dosen</h6>
+    <a href="index.php">Kembali</a>
+</body>
+</html>
